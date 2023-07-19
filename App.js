7a34bd7250464b4f17048532dbff84370ddd7c0b
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-07-19 20:53:20 +0530

    add js

--- App.js
+++ App.js
@@ -9,18 +9,23 @@ const rt = ReactDOM.createRoot(document.getElementById("root"));
 
 rt.render(heading);
 
+// children 1
 const heading1 = React.createElement(
   "h1",
   { id: "container" },
   "This is h1 tag from bodyContainer 😋"
 );
 
+// children 2
 const heading2 = React.createElement(
   "h2",
   { id: "container" },
   "This is h2 tag from bodyContainer 😎"
 );
 
+// When we want to pass more than 1 tag
+// 1st argument is tag name 2nd argument is attribute (Props) & 3rd argument is children
+// Props is besically properties it can be anything like 'id' , 'class' or anything'
 const bodyContainer = React.createElement("div", { id: "bodyContainer" }, [
   heading1,
   heading2,
